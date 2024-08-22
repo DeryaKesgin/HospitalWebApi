abc826ad9dbd97dc197770621d3ab838e7387880
DeryaKesgin <99399615+DeryaKesgin@users.noreply.github.com>
2024-08-22 17:46:22 +0300

    stil

--- webapi/webapi/obj/Debug/net8.0/webapi.AssemblyInfo.cs
+++ webapi/webapi/obj/Debug/net8.0/webapi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("webapi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+205851b88877c8671e8a13132eacb4f7d3594d5a")]
 [assembly: System.Reflection.AssemblyProductAttribute("webapi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("webapi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
